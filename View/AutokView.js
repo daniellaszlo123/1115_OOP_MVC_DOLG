4d4c884a621fa28f8bc74714cd9868dfeae41e92
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-11-15 10:17:25 +0100

    felesleg torolve

--- View/AutokView.js
+++ View/AutokView.js
@@ -4,14 +4,13 @@ class AutokView{
         this.container=szuloelem;
 
         //console.log(tomb);
-        this.autokBetolt(tomb)
-    }
-
-    autokBetolt(tomb){
-        this.container.html("");
         tomb.forEach(auto => {
             new AutoView(auto, this.container);
         });
+    }
+
+    autokBetolt(){
+        
     }
 }
 export default AutokView;
